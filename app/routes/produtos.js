aba5d9cfafeb6535160cfdb654ba7a2e285cb7e8
Ramón Souza <contato@ramonsouza.com>
2018-10-02 09:03:22 -0300

    conexao mysql local + query basica retornando json com livros

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,18 @@
 module.exports = function(app){
     app.get('/produtos', function(req,res){
-        res.render("produtos/lista");
+        var mysql = require('mysql');
+        var connect = mysql.createConnection({
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'casadocodigo'
+        });
+
+        connect.query('select * from livros', function(err, results){
+            res.send(results);
+        });
+
+        connect.end();
     });
     
 }
