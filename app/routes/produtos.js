6e288e01639e5eb8ab354152aef24141639eb2b0
Ramón Souza <contato@ramonsouza.com>
2018-10-02 19:01:37 -0300

    adicionando body-parser e fazendo funcionalidade insert

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,9 +1,9 @@
 module.exports = function(app){
     app.get('/produtos', function(req,res){
         var connect = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connect);
+        var produtosDAO = new app.infra.ProdutosDAO(connect);
 
-        produtosBanco.lista(function(err, results){
+        produtosDAO.lista(function(err, results){
             res.render('produtos/lista', {lista: results});
         });
 
@@ -12,5 +12,15 @@ module.exports = function(app){
     
     app.get('/produtos/form',function(req,res){
         res.render('produtos/form');
+    });
+
+    app.post('/produtos/salva',function(req,res){
+        var produto = req.body;
+
+        var connect = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connect);
+        produtosDAO.salva(produto,function(err,result){
+            res.redirect('/produtos');
+        });
     });
 }
