75a509089f771e04a04c33ee60c5af46a030d7f4
Ramón Souza <contato@ramonsouza.com>
2018-10-25 15:16:53 -0300

    primeiro teste real com mocha

--- test/produtos.js
+++ test/produtos.js
@@ -1,5 +1,23 @@
+var http = require('http');
+
 describe('#ProdutosController',function(){
-    it('#listagem json', function(){
-        console.log("teste de verificação de listagem json");
+    it('#listagem json', function(done){
+        var configuracoes = {
+            hostname: '127.0.0.1',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+        http.get(configuracoes, function(res){
+            if(res.statusCode == 200){
+                console.log("status ok");
+            }
+            if(res.headers['content-type'] == 'application/json; charset=utf-8'){
+                console.log("content type ok");
+            }
+            done();
+        });
     });
 });
