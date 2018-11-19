d51e3c116d27d197ba4f26e08c13c65ab272dad3
Ramón Souza <contato@ramonsouza.com>
2018-11-19 21:59:31 -0200

    alterando teste para usar lib 'supertest' (testes sem precisar subir o servidor node e codigo mais limpo)

--- test/produtos.js
+++ test/produtos.js
@@ -1,20 +1,11 @@
-var http = require('http');
-var assert = require('assert');
+var express = require('../config/express')();
+var request = require('supertest')(express);
 
-describe('#ProdutosController',function(){
-    it('#listagem json', function(done){
-        var configuracoes = {
-            hostname: '127.0.0.1',
-            port: 3000,
-            path: '/produtos',
-            headers: {
-                'Accept': 'application/json'
-            }
-        };
-        http.get(configuracoes, function(res){
-            assert.equal(res.statusCode,200);
-            assert.equal(res.headers['content-type'],'application/json; charset=utf-8');
-            done();
-        });
+describe('ProdutosController',function(){
+    it('listagem json', function(done){
+        request.get('/produtos')
+        .set('Accept', 'application/json')
+        .expect('Content-Type',/json/)
+        .expect(200,done);
     });
 });
