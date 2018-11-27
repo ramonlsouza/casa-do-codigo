56959ad43710d68fa8a4e0b410f6a2a96411f70e
Ramón Souza <contato@ramonsouza.com>
2018-11-27 21:27:40 -0200

    criando middlewares para tratar erros 404 e 500 (erro 500 com diferentes respostas em produção e dev)

--- config/express.js
+++ config/express.js
@@ -21,9 +21,25 @@ module.exports = function(){
     app.use(bodyParser.json());
     app.use(expressValidator());
 
+
     load('routes', {cwd: 'app'})
         .then('infra')
         .into(app);
+
+    //ordem do middleware é importante
+    //esse precisa ir depois de carregar rotas!
+    app.use(function(req,res,next){
+        res.status(404).render('erros/404');
+        next();
+    });
     
+    app.use(function(error,req,res,next){
+        if(process.env.NODE_ENV == 'production'){
+            res.status(500).render('erros/500');
+            return;
+        }
+        next(error);
+    });
+
     return app;
 }
