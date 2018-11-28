b0c0d6faf28b7347e0f87d38cc405a07a9e4a318
Ramón Souza <contato@ramonsouza.com>
2018-11-27 22:12:27 -0200

    configuracoes extras para deploy

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var io = require('socket.io')(http);
 
 app.set('io',io);
 
-http.listen(3000, function(){
+var porta = process.env.PORT || 3000;
+
+http.listen(porta, function(){
     console.log("servidor rodando");
 });
